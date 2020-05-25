63bd93268bdbff61064dd42830d8693c82320f64
Денис <dvzuboff@gmail.com>
2020-05-25 17:05:30 +0300

    3.2. Фиксация изменений на github [#295628]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+            for (int x = 1; x <= 9; x++) {
+            System.out.println("1*"+x+"="+x);
+        }
+    }
+}
