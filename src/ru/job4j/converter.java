5697ed232b473e54f351c1e1d22dad97e2c1e981
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-06-02 15:18:39 +0300

    Add files via upload

---
+++ src/ru/job4j/converter.java
@@ -0,0 +1,23 @@
+package ru.job4j.Converter;
+
+public class converter {
+
+    public static int rubleToEuro (int value) {
+
+        int rsl = value / 70;
+        return rsl;
+    }
+    public static int rubleToDollars (int value) {
+
+        int rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+            int euro = converter.rubleToEuro(140);
+            System.out.println("140 rubles are " + euro + " euro.");
+            int dollars = converter.rubleToDollars(180);
+            System.out.println("180 rubles are " + dollars + " dollars.");
+   }
+
+}
