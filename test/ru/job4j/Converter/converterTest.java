e121c3257fe6772bc105e55914a85e16f969bff8
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-06-06 14:06:18 +0300

    Add files via upload

---
+++ test/ru/job4j/Converter/converterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.Converter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class converterTest {
+
+    @Test
+    public void When140rubleTo2Euro() {
+    }
+
+    @Test
+    public void When140rubleTo3Dollars() {
+    }
+}
