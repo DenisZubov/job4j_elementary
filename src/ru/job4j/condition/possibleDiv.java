5fa6ba261116b82ed86580a0c345d2fc374ea71b
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-06-15 16:29:42 +0300

    Add files via upload

--- src/ru/job4j/condition/possibleDiv.java
+++ src/ru/job4j/condition/possibleDiv.java
@@ -5,10 +5,13 @@ public class possibleDiv {
         if (number < 0) {
             System.out.println("This is negative numbers.");
         }
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
                    }
 
     public static void main(String[] args) {
-        Div(-5);
+        Div(0);
     }
 }
 
