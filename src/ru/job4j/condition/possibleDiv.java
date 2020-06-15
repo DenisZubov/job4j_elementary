50d6fd5da3f8846a0150216e307124375f1c9658
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-06-15 16:24:20 +0300

    Add files via upload

--- src/ru/job4j/condition/possibleDiv.java
+++ src/ru/job4j/condition/possibleDiv.java
@@ -2,13 +2,13 @@ package ru.job4j.condition;
 
 public class possibleDiv {
     public static void Div(int number) {
-        if (number == 0) {
+        if (number < 0) {
             System.out.println("This is negative numbers.");
         }
-    }
+                   }
 
     public static void main(String[] args) {
-        Div(0);
+        Div(-5);
     }
 }
 
