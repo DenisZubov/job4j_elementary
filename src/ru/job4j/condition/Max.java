4a45bd92b0b3e3c70e72e56a1dd0bf1e6bba177c
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-06-17 22:27:45 +0300

    Add files via upload

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int can = left > right ? left:right;
-        return can;
+        return left >= right ? left : right;
     }
     public static void main(String[] args) {
         int msg = max(52, 56);
