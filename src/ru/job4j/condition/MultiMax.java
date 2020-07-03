a4d7d6d72fdbec49d561cd2642a27468c0dd97c1
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-03 08:31:34 +0300

    Add files via upload

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int a, int b, int c) {
-        int d = a>b ? (d=a):(d=b);
-        int e = d > c ? (e=d) : (e=c);
-        return e;
+        int result = a > b ? a : b;
+        result = result > c ? result  : c;
+        return result;
     }
     public static void main(String[] args) {
-        int msg = MultiMax.max(8, 7, 9);
+        int msg = MultiMax.max(8, 7, 10);
         System.out.println(msg);
     }
 }
