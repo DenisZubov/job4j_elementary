028f29d9ff995aba4041f227eb5b2d8b39829aa2
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-03 08:25:54 +0300

    Add files via upload

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -3,14 +3,13 @@ package ru.job4j.condition;
 public class Triangle {
     static boolean exist (double ab, double ac, double bc){
         if (ab + ac > bc&&ac + bc > ab&&ab + bc > ac) {
-            return true;
-        }
+            return ab + ac > bc && ac + bc > ab && ab + bc > ac;        }
         else {
             return false;
         }
     }
 
     public static void main(String[] args){
-        System.out.println( exist(1,1,1) );
+        System.out.println( exist(1,2,2) );
     }
 }
