ac7e9182ed780dced7d03121aeee052954a361f2
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-13 12:47:11 +0300

    Add files via upload

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,25 +1,21 @@
 package ru.job4j.loop;
 
 public class Counter {
-    public static int sum(int start, int finish) {
-        int sum = 0;
-        for (start=0; start <= finish; start++) {
-            sum = sum + start;
-        }
-        return sum;
-    }
-    /** @noinspection checkstyle:NoWhitespaceBefore*/
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for ( start = 1; start <= finish; start++ )
+        int i = start;
+        for ( start = i; start <= finish; start++ )
             if (start % 2 == 0) {
                 sum = sum + start;
             }
         return sum;
+
     }
+
     public static void main(String[] args) {
-        System.out.println(sum(2, 11));
-        System.out.println(sum(0, 124));
-        System.out.println(sum(12, 1002));
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
+
     }
 }
