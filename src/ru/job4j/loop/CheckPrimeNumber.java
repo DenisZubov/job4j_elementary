b54b82053d7fbb9ab1cfb4b0dd95ff3d4757642c
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-15 18:22:15 +0300

    Add files via upload

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,17 +2,18 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
 
-            public static boolean check(int n) {
-                boolean prime = true;
-                for (int i = 2; i < n; i++) {
-                    if (n % i == 0) {
-                        prime = false;
-                    }
-                }
-                return prime;
+    public static boolean check(int n) {
+        boolean prime = true;
+        prime = n > 1;{
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0 )
+                prime = false;
             }
-            public static void main (String args[]) {
+        }
+        return prime;
+    }
+    public static void main (String args[]) {
 
-                System.out.println(check(4));
+        System.out.println(check(6));
     }
 }
