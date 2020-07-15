d1de98087017db3e98bf1317aeff850446c1afef
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-15 15:56:17 +0300

    Add files via upload

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+            public static boolean check(int n) {
+                boolean prime = true;
+                for (int i = 2; i < n; i++) {
+                    if (n % i == 0) {
+                        prime = false;
+                    }
+                }
+                return prime;
+            }
+            public static void main (String args[]) {
+        System.out.println(check(8));
+    }
+}
