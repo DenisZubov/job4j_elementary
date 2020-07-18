929d61a6f62e8e41c0b4c1330d4d4fabfe61f8df
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-18 14:31:42 +0300

    Add files via upload

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    @SuppressWarnings("checkstyle:LeftCurly")
+    public static void main (String[]args)  {
+      short[] ages = new short[10];
+      String[] surnames = new String [100500];
+      float[] rrices = new float[40];
+
+    }
+}
