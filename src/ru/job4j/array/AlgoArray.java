58ef379e747487498023e9dc80ae832157ce3807
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-20 20:06:10 +0300

    Add files via upload

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -4,14 +4,11 @@ public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
         int temp = array[0];
-        //array[0] = array[3];
-        //array[3] = temp;
 
         for (int index = 0; index < array.length; index++) {
             for (int i = 0; i < array.length; i++) {
                 if (array[index] < array[i]) {
                     temp = array[index];
-                    array[index] = temp;
                     array[index] = array[i];
                     array[i] = temp;
                 }
