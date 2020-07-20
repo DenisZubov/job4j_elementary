aba25b5bf1395a8c919c6ad77ad26c4db4357f52
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-20 17:10:50 +0300

    Add files via upload

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,3 +1,4 @@
+
 package ru.job4j.array;
 
 public class FindLoop {
@@ -6,14 +7,14 @@ public class FindLoop {
         for (int index = 0; index<=data.length; index++) {
             if (data[index] == el) {
                 rst = index;
-                return rst;
+                break;
             }
         }
         return rst;
     }
     public static void main(String[] args){
-        int value = 4;
+        int value = 2;
         int[] xxx = {1, 2, 3, 4};
         System.out.println (FindLoop.indexOf(xxx, value));
     }
-    }
+}
