71d2bee2a50d5b3146d75c227379310e66089864
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-21 11:39:08 +0300

    Add files via upload

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -2,8 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] swap(int[] array) {
-        int i = array.length / 2;
-        for (i = 0; i < array.length / 2; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length-i-1];
             array[array.length-i-1] = temp;
@@ -11,9 +10,9 @@ public class Turn {
         return array;
     }
     public static void main ( String[]args){
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
         int[] rsl = swap(nums);
-        for (int i = 0; i <= rsl.length; i++) {
+        for (int i = 0; i < rsl.length; i++) {
             System.out.println(rsl[i]);
         }
     }
