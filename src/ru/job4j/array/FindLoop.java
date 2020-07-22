93e3f2e4324657e65ed4b69a1d68eebc87b15ce4
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-22 19:23:39 +0300

    Add files via upload

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -4,7 +4,7 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int index = 0; index<data.length; index++) {
+        for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
@@ -12,9 +12,20 @@ public class FindLoop {
         }
         return rst;
     }
+    public static boolean numOfDip(int[] data, int el, int start, int finish){
+            boolean dip = false;
+            for (int index = start; index <= finish; index++) {
+                if (data[index] == el) {
+                    dip = true;
+                }
+            }
+        return dip;
+    }
     public static void main(String[] args){
         int value = 3;
         int[] xxx = {1, 2, 3, 4};
         System.out.println (FindLoop.indexOf(xxx, value));
-    }
+        System.out.println (FindLoop.numOfDip(xxx, 3, 0, 1));
+
+            }
 }
