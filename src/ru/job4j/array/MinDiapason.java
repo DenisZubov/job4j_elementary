0f49fb8de82256bea518c16b8e380931ce89dd89
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-23 13:49:41 +0300

    Add files via upload

--- src/ru/job4j/array/MinDiapason.java
+++ src/ru/job4j/array/MinDiapason.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class MinDiapason {
     public static int min(int[] data, int start, int finish) {
         int minn = data[start];
-        for (int i = start - 1; i <= finish - 1; i++) {
+        for (int i = start; i <= finish; i++) {
             if (minn > data[i]) {
                 minn = data[i];
             }
@@ -12,7 +12,7 @@ public class MinDiapason {
     }
     public static void main(String[] args) {
         int[] array = {0, 4, 2, 1};
-        System.out.println(MinDiapason.min(array, 1, 3));
+        System.out.println(MinDiapason.min(array, 2, 3));
     }
 
 }
