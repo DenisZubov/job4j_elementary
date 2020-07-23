c6fe2c909f5ef8d96ff105ec176923b19558d4e4
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-23 09:36:42 +0300

    Add files via upload

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -1,19 +1,20 @@
 package ru.job4j.array;
 
 public class EndsWith {
-    public static boolean EndWith (char[] word, char[] pref) {
+    public static boolean ends(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 1; i<=pref.length; i++) {
-            if (word[word.length-i] != pref[pref.length-i]) {
+        for (int i = 1; i <= pref.length; i++) {
+            if (word[word.length - i] != pref[pref.length - i]) {
                 result = false;
+                break;
             }
         }
         return result;
     }
-    public static void main ( String[]args){
-        char[] x = {'H', 'e', 'l', 'l', 'o'};
+    public static void main(String[]args)
+      {char[] x = {'H', 'e', 'l', 'l', 'o'};
         char[] y = {'l', 'o'};
-        boolean rsl = EndWith (x,y);
+        boolean rsl = ends (x, y);
         System.out.println(rsl);
     }
 }
