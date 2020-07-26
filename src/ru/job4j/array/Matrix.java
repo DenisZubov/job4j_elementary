6656ab21d75b021bb32294d76b205fdb39bc2c59
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-26 09:27:50 +0300

    Add files via upload

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -1,16 +1,16 @@
 package ru.job4j.array;
 
 public class Matrix {
-    public static int[][] multiple(int size){
+    public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                table[i][j] = (i+1)*(j+1);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
             }
         }
         return table;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[][] value = Matrix.multiple(9);
         int x = value[1][1];
         System.out.println(x);
