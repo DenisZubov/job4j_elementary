9c462789284fff4245f41b472ed0fa761235c301
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-30 12:22:15 +0300

    Add files via upload

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -12,6 +12,18 @@ public class MatrixCheck {
         return result;
     }
 
+    public static boolean monoVertical (char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][row] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         char[][] array = {
                 {'h', 'h', 'h'},
