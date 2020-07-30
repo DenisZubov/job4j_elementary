e60217f59e664f403c20e90c183f87152f4eccba
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-07-30 10:39:30 +0300

    Add files via upload

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,23 +4,21 @@ public class MatrixCheck {
     public static boolean monoHorizontal(int[][] board, int row) {
         boolean result = false;
         int k = 0;
-        for (int j = 0; j < board.length; j++) {
-            for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
                 if(board[i][j] == row) {
                     k++;
                     if (k==board.length+1) {
                         result = true;
                     }
                 }
+                return result;
             }
-        }
-        return result;
     }
     public static void main(String[] args) {
         int[][] array = {
-                {1, 2, 3},
-                {2, 3, 3},
-                {1, 2, 2}};
+                {1, 1, 1},
+                {3, 3, 3},
+                {6, 7, 7}};
         boolean rsl = monoHorizontal(array, 3);
         System.out.println(rsl);
     }
