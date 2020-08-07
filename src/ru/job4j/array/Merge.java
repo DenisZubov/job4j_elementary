4858aaf2ff9b93c7aa4903c58db0a7b348c5cb3c
DenisZubov <66013369+DenisZubov@users.noreply.github.com>
2020-08-07 08:20:39 +0300

    Add files via upload

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static void main(String[] args) {
+        int[]a = {1, 2, 3, 4, 5};
+        int[]b = {6, 7, 8, 9, 10};
+        int[]c = new int[a.length + b.length];
+        int x = 0;
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i];
+            x++;
+        }
+        for (int j = 0;j < b.length;j++) {
+            c[x++] = b[j];
+        }
+        for (int i = 0; i < c.length; i++){
+        System.out.println(c[i]);}
+    }
+}
+
